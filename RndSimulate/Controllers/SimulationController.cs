9cfe2be8762d095f3963a4227c878dc0dc944185
Khalil <52969012+khail-k@users.noreply.github.com>
2022-12-01 19:11:10 +0000

    Add single threaded job

--- RndSimulate/Controllers/SimulationController.cs
+++ RndSimulate/Controllers/SimulationController.cs
@@ -9,4 +9,22 @@ public class SimulationController : ControllerBase{
     public void Sim(){
         SimulateService.Simulate();
     }
+
+    [HttpGet("start")]
+    public void Start(){
+        // Tries to start the sim runner
+        SimulateService.Start();
+    }
+
+    [HttpGet("progress")]
+    public string Progress(){
+        // state of runner
+        return SimulateService.Progress();
+    }
+
+    [HttpGet("result")]
+    public bool Result(){
+        // Returns if the sim is running
+        return SimulateService.isRunning();
+    }
 }
--- RndSimulate/Services/SimulationService.cs
+++ RndSimulate/Services/SimulationService.cs
@@ -4,8 +4,32 @@ namespace Simulate.Services;
 
 public static class SimulateService{
     private static readonly Random rnd = new Random();
+    private static ThreadStart tstart = new ThreadStart(Simulate);
+    private static Thread thread;
+
+    static SimulateService(){
+        // instantiate thread, saves adding error handling
+        thread = new Thread(()=>{});
+    }
 
     public static void Simulate(){
         System.Threading.Thread.Sleep(rnd.Next(0,10) * 1000);
+    }
+
+    public static void Start(){
+        if (!isRunning()){
+            thread = new Thread(tstart);
+            thread.Start();
+        }
+    }
+
+    public static string Progress(){
+        Debug.Assert(thread != null);
+        return thread.ThreadState.ToString();
+    }
+
+    public static bool isRunning(){
+        Debug.Assert(thread != null);
+        return thread.IsAlive;
     }
 }
